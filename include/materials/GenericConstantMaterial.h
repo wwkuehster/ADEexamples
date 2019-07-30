3f080934f0c6f96fe214808eb25c4f3b95baa8ef
wwkuehster <52732530+wwkuehster@users.noreply.github.com>
2019-07-30 09:55:57 -0500

    Add files via upload

---
+++ include/materials/GenericConstantMaterial.h
@@ -0,0 +1,44 @@
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#pragma once
+
+#include "Material.h"
+
+// Forward Declarations
+class GenericConstantMaterial;
+
+template <>
+InputParameters validParams<GenericConstantMaterial>();
+
+/**
+ * This material automatically declares as material properties whatever is passed to it
+ * through the parameters 'prop_names' and uses the values from 'prop_values' as the values
+ * for those properties.
+ *
+ * This is not meant to be used in a production capacity... and instead is meant to be used
+ * during development phases for ultimate flexibility.
+ */
+class GenericConstantMaterial : public Material
+{
+public:
+  GenericConstantMaterial(const InputParameters & parameters);
+
+protected:
+  virtual void initQpStatefulProperties() override;
+  virtual void computeQpProperties() override;
+
+  std::vector<std::string> _prop_names;
+  const std::vector<Real> & _prop_values;
+
+  unsigned int _num_props;
+
+  std::vector<MaterialProperty<Real> *> _properties;
+};
+
